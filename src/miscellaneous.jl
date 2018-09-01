d2c4d2ec8f85bc28fd348155599f56197d38912f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-01 07:28:59 -0400

    RoundDown: cld->fld, add RoundNearest using div

--- src/miscellaneous.jl
+++ src/miscellaneous.jl
@@ -5,7 +5,9 @@ function divide_evenly(n, d)
     num_splits, divrem(n, num_splits)
 end
 
-round_x_to_nearest_y(x, y) = round(Int, x / y) * y
 round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Up}) = cld(x,y)*y
-round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Down}) = cld(x,y)*y
+round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Down}) = fld(x,y)*y
+round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Nearest}) = div(x,y)*y
+
+round_x_to_nearest_y(x, y) = round(Int, x / y) * y
 round_x_to_nearest_y(x, y::Int, rm::RoundingMode) = round(Int, x / y, rm) * y
