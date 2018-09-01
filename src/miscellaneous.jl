afb25af5897bc0c543e4f47682ab352e94285022
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-01 15:43:37 -0400

    per request: additional method signature

--- src/miscellaneous.jl
+++ src/miscellaneous.jl
@@ -11,3 +11,4 @@ round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Nearest}) = div(x,y)*y
 
 round_x_to_nearest_y(x, y) = round(Int, x / y) * y
 round_x_to_nearest_y(x, y::Int, rm::RoundingMode) = round(Int, x / y, rm) * y
+round_x_to_nearest_y(x, y, ::RoundingMode{:Nearest}) = round(Int, x / y) * y
