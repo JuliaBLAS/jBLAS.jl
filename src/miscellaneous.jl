18a8608019a81d4acfabbdb12904252fd420ec2f
chriselrod <elrodc@gmail.com>
2018-09-03 11:28:43 -0500

    Fix bug in method round_x_to_nearest_y(::Int, ::Int, ::RoundingMode{:Nearest})

--- src/miscellaneous.jl
+++ src/miscellaneous.jl
@@ -7,7 +7,7 @@ end
 
 round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Up}) = cld(x,y)*y
 round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Down}) = fld(x,y)*y
-round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Nearest}) = div(x,y)*y
+round_x_to_nearest_y(x::Int, y::Int, ::RoundingMode{:Nearest}) = round(Int, x / y) * y
 
 round_x_to_nearest_y(x, y) = round(Int, x / y) * y
 round_x_to_nearest_y(x, y::Int, rm::RoundingMode) = round(Int, x / y, rm) * y
